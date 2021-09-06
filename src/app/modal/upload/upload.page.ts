48c398c11784f351b784014a3f245419268f988c
youkiery <youkiery@gmail.com>
2021-09-06 19:19:11 +0700

    tải ảnh spa

--- src/app/modal/upload/upload.page.ts
+++ src/app/modal/upload/upload.page.ts
@@ -1,15 +1,149 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AlertController } from '@ionic/angular';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.page.html',
   styleUrls: ['./upload.page.scss'],
 })
-export class UploadPage implements OnInit {
+export class UploadPage {
+  public option = [{ name: "wash_dog", value: "Tắm chó", check: false }, { name: "wash_cat", value: "Tắm mèo", check: false }, { name: "wash_white", value: "Tắm trắng", check: false }, { name: "cut_fur", value: "Cắt lông", check: false }, { name: "shave_foot", value: "Cạo lông chân", check: false }, { name: "shave_fur", value: "Cạo ông", check: false }, { name: "cut_claw", value: "Cắt, dũa móng", check: false }, { name: "cut_curly", value: "Cắt lông rối", check: false }, { name: "wash_ear", value: "Vệ sinh tai", check: false }, { name: "wash_mouth", value: "Vệ sinh răng miệng", check: false }, { name: "paint_footear", value: "Nhuộm chân, tai", check: false }, { name: "paint_all", value: "Nhuộm toàn thân", check: false }, { name: "pin_ear", value: "Bấm lỗ tai", check: false }, { name: "cut_ear", value: "Cắt lông tai", check: false }, { name: "dismell", value: "Vắt tuyết hôi", check: false }]
+  public max = 640
+  public count = 0
+  @ViewChild('pwaphoto') pwaphoto: ElementRef;
+  constructor(
+    public rest: RestService,
+    public alert: AlertController,
+    private storage: AngularFireStorage,
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ionViewDidEnter() {
+    if (!this.rest.action.length) this.rest.navCtrl.navigateRoot('home')
   }
 
+  public suggest() {
+    this.rest.router.navigateByUrl('/modal/suggest')
+  }
+
+  public upload() {
+    this.pwaphoto.nativeElement.click();
+  }
+
+  public remove(i: number) {
+    this.rest.temp.image = this.rest.temp.image.filter((item: any, index: number) => {
+      return index !== i
+    })
+  }
+
+  public async uploadPWA() {
+    const fileList: FileList = this.pwaphoto.nativeElement.files;
+    if (fileList && fileList.length > 0) {
+     await this.rest.freeze('Đang tải...')
+      for (let i = 0; i < fileList.length; i++) {
+        await this.firstFileToBase64(fileList[i]).then((result: string) => {
+          let image = new Image();
+          image.src = result
+          image.onload = () => {
+            let canvas = document.createElement('canvas')
+            let context = canvas.getContext('2d')
+            let rate = 1
+            if (image.width > this.max || image.height > this.max) {
+              if (image.width > image.height) rate = image.width / this.max
+              else rate = image.height / this.max
+            }
+            let newWidth = image.width / rate
+            let newHeight = image.height / rate
+            canvas.width = newWidth
+            canvas.height = newHeight
+            context.drawImage(image, 0, 0, canvas.width, canvas.height)
+            this.rest.temp.image.push(canvas.toDataURL('image/jpeg'));
+          }
+        }, (err: any) => { });
+      }
+      this.rest.defreeze()
+    }
+  }
+
+  private firstFileToBase64(fileImage: File): Promise<{}> {
+    return new Promise((resolve, reject) => {
+      let fileReader: FileReader = new FileReader();
+      if (fileReader && fileImage != null) {
+        fileReader.readAsDataURL(fileImage);
+        fileReader.onload = () => {
+          resolve(fileReader.result);
+        };
+
+        fileReader.onerror = (error) => {
+          reject(error);
+        };
+      } else {
+        reject(new Error('No file found'));
+      }
+    });
+  }
+
+
+  public uploadImage(image: string) {
+    return new Promise((resolve) => {
+      const path = 'images/' + new Date().getTime() + '.jpg';
+      let fileRef = this.storage.ref(path);
+      let base64 = image.substr(image.indexOf(',') + 1);
+      let metadata = {
+        contentType: 'image/jpeg',
+      };
+
+      fileRef.putString(base64, 'base64', metadata).then((response) => {
+        fileRef.getDownloadURL().subscribe(url => {
+          resolve(url)
+        })
+      }, (error) => {
+        resolve('')
+      })
+    })
+  }
+
+  public async checkSaveSubmit() {
+    this.count++
+    if (this.rest.temp.image.length == this.count) {
+      this.insertSubmit()
+    }
+  }
+
+  public async insert() {
+    this.count = 0
+    this.rest.freeze('Đang tải ảnh...')
+    
+    if (!this.rest.temp.name.length) this.rest.notify('Chưa nhập tên khách hàng')
+    else if (!this.rest.temp.phone.length) this.rest.notify('Chưa nhập số điện thoại khách')
+    else if (!this.rest.temp.image.length) this.insertSubmit()
+    else this.rest.temp.image.forEach((image, index) => {
+      if (image.length > 200) {
+        this.uploadImage(image).then((url: string) => {
+          this.rest.temp.image[index] = url.replace('%2F', '@@')
+          this.checkSaveSubmit()
+        })
+      }
+      else this.checkSaveSubmit()
+    });
+  }
+
+  public checkOption() {
+    let option = {}
+    this.option.forEach(item => {
+      option[item.name] = Number(item.check)
+    });
+    return option
+  }
+
+  public async insertSubmit() {
+    this.rest.temp.option = this.checkOption()
+    this.rest.checkpost('spa', 'insert', this.rest.temp).then(response => {
+      this.rest.navCtrl.pop()
+      this.rest.defreeze()
+    }, () => {
+      this.rest.defreeze()
+    })
+  }
 }
--- src/app/module/spa/spa.page.ts
+++ src/app/module/spa/spa.page.ts
@@ -1,90 +1,90 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
 import { RestService } from 'src/app/services/rest.service';
 import { TimeService } from 'src/app/services/time.service';
+
+
+@Component({
+  selector: 'modal-spa',
+  template: `
+    <ion-toolbar color="success">
+      <ion-button color="light" fill="clear" (click)="modal.dismiss()">
+        <ion-icon name="chevron-back-outline"></ion-icon>
+      </ion-button>
+    </ion-toolbar>
+
+    <ion-content>
+      <img [src]="rest.temp">
+    </ion-content>
+  `
+})
+export class SpaModal {
+  constructor(
+    public rest: RestService,
+    public modal: ModalController
+  ) { }
+}
 
 @Component({
   selector: 'app-spa',
   templateUrl: './spa.page.html',
   styleUrls: ['./spa.page.scss'],
 })
-export class SpaPage implements OnInit {
+export class SpaPage {
   interval = null
   checked = false
   constructor(
     public rest: RestService,
     public time: TimeService,
-    public alert: AlertController
+    public alert: AlertController,
+    public modal: ModalController
   ) { }
 
-  ngOnInit() { }
-
   async ionViewDidEnter() {
-    // loading spa list on start
-    await this.rest.freeze('Lấy danh sách spa')
-    return new Promise((resolve) => {
-      this.filter().then(() => {
-        this.rest.defreeze()
-        resolve(true)
-      })      
-    }).then(() => {
-      // auto refresh every 5 second
-      // if not got any data, no other post send
-      this.interval = setInterval(() => {
-        if (!this.checked) {
-          this.filter()
-        }
-      }, 5000)
+    this.rest.ready().then(() => {
+      if (!this.rest.data.spa.init) {
+        this.rest.data.spa.time = (new Date()).getTime()
+        this.filter()
+      }
     })
   }
 
-  ionViewDidLeave() {
-    clearInterval(this.interval)
+  public async filter() {
+    await this.rest.freeze('Đang tải danh sách')
+    this.rest.checkpost('spa', 'auto', {
+      action: 'spa-auto',
+      time: this.rest.data.spa.time
+    }).then((resp) => {
+      this.rest.data.spa.list = resp.list
+      this.rest.defreeze()
+    }, () => {
+      this.rest.defreeze()
+    })
+  }
+
+  public async detail(image: string) {
+    this.rest.temp = image
+    let modal = await this.modal.create({
+      component: SpaModal
+    })
+    await modal.present()
   }
 
   public insert() {
-    if (this.rest.config.module.spa < 2) this.rest.notify('Chưa cấp quyền truy cập')
-    else {
-      this.rest.data.spa.edit = {
-        id: 0,
-        name: '',
-        phone: '',
-        note: '',
-        image: [],
-        type: JSON.parse(JSON.stringify(this.rest.data.spa.type))
-      }
-      this.rest.router.navigateByUrl('/spa/insert')
+    this.rest.temp = {
+      id: 0,
+      name: '',
+      phone: '',
+      note: '',
+      image: [],
+      time: this.rest.data.spa.time
     }
-  }
-
-  public detail(id: number) {
-    this.rest.checkpost('spa', 'get', {
-      id: id
-    }).then(response => {
-      this.rest.data.spa.edit = response.data
-      this.rest.router.navigateByUrl('/spa/insert')
-    })
-  }
-
-  public async done(id: number) {
-    if (this.rest.config.module.spa < 2) this.rest.notify('Chưa cấp quyền truy cập')
-    else {
-      await this.rest.freeze('Đang hoàn thành')
-      this.rest.checkpost('spa', 'done', {
-        id: id,
-        current: this.time.datetotime(this.rest.data.spa.current)
-      }).then((response) => {
-        this.rest.data.spa.time = response.time
-        this.rest.data.spa.data = response.data
-        this.rest.defreeze()
-      }, () => {
-        this.rest.defreeze()
-      })
-    }
+    this.rest.action = 'spa'
+    this.rest.router.navigateByUrl('/upload')
   }
 
   public async pickDate() {
-    let current = this.rest.data.spa.current.split('/')
+    let current = this.time.timetodate(this.rest.data.spa.time).split('/')
     let target = current[2] + '-' + current[1] + '-' + current[0]
     let alert = await this.alert.create({
       header: 'Chọn ngày',
@@ -106,8 +106,8 @@ export class SpaPage implements OnInit {
           cssClass: 'secondary',
           handler: (e) => {
             let result = e.date.split('-')
-            this.rest.data.spa.current = result[2] + '/' + result[1] + '/' + result[0]
-            this.filterPickDate()
+            this.rest.data.spa.time = this.time.datetotime(result[2] + '/' + result[1] + '/' + result[0])
+            this.filter()
           }
         }
       ]
@@ -115,41 +115,9 @@ export class SpaPage implements OnInit {
     alert.present()
   }
 
-  public async filterPickDate() {
-    await this.rest.freeze('Lấy danh sách spa')
-    this.filter().then(() => {
-      this.rest.defreeze()
-    })      
-  }
-
-  public filter() {
-    return new Promise((resolve) => {
-      this.checked = true
-      this.rest.checkpost('spa', 'auto', {
-        action: 'spa-auto',
-        time: this.rest.data.spa.time,
-        current: this.time.datetotime(this.rest.data.spa.current)
-      }).then((response) => {
-        if (response.data && response.data != this.rest.data.spa.data) {
-          this.rest.data.spa.data = response.data
-          this.rest.data.spa.time = response.time
-        }
-        this.checked = false
-        resolve(true)
-      }, () => {
-        this.checked = false
-        resolve(true)
-      })
-    })
-  }
-
   public async changeDate(amount: number) {
-    let time = this.time.datetotime(this.rest.data.spa.current) + amount * 60 * 60 * 24 * 1000
-    this.rest.data.spa.current = this.time.timetodate(time)
-    await this.rest.freeze('Lấy danh sách spa')
-    this.filter().then(() => {
-      this.rest.defreeze()
-    })
+    this.rest.data.spa.time += amount * 60 * 60 * 24 * 1000
+    this.filter()
   }
 }
 
