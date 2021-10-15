1098819521d90068a94a77c5ae83c6611b3273de
youkiery <youkiery@gmail.com>
2021-10-15 14:20:46 +0700

    thêm modal quản lý

---
+++ src/app/module/usg/usgmanager/usgmanager.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usgmanager',
+  templateUrl: './usgmanager.page.html',
+  styleUrls: ['./usgmanager.page.scss'],
+})
+export class UsgmanagerPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
