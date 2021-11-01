21544874edd5369dfec0f4a816d5c9882a79d741
youkiery <youkiery@gmail.com>
2021-11-01 17:04:21 +0700

    lái xe

--- src/app/module/ride/ride.page.ts
+++ src/app/module/ride/ride.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-ride',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RidePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public rest: RestService
+  ) { }
 
   ngOnInit() {
+  }
+
+  public async ionViewWillEnter() {
+    this.rest.ready().then(() => {
+      this.init()
+    })
+  }
+
+  public async init() {
+    await this.rest.freeze('Đang lấy danh sách..')
+    this.rest.checkpost('ride', 'init', { }).then(resp => {
+      this.rest.defreeze()
+      this.rest.ride.list = resp.list
+      this.rest.ride.from = resp.from
+      this.rest.ride.end = resp.end
+    }, () => {
+      this.rest.defreeze()
+    })
   }
 
 }
